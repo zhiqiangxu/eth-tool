a170d23232b672cc1b298ce31fa82d61efa3a3bf
zhiqiangxu <652732310@qq.com>
2023-08-28 17:41:43 +0800

    add deposit.gen

--- src/beacon/keygen.rs
+++ src/beacon/keygen.rs
@@ -1,8 +1,13 @@
+use std::str::FromStr;
+
 use clap::Parser;
 
 use hex;
 
-use bls::{blst_implementations::SecretKey, Keypair};
+use bls::{
+    blst_implementations::{PublicKey, PublicKeyBytes, SecretKey},
+    Keypair,
+};
 #[derive(Parser, Debug)]
 pub struct Keygen {}
 
@@ -11,6 +16,19 @@ impl Keygen {
         let keypair = Keypair::random();
         let sk = SecretKey::deserialize(keypair.sk.serialize().as_bytes()).unwrap();
         assert_eq!(sk.public_key().as_hex_string(), keypair.pk.as_hex_string());
+        assert_eq!(
+            PublicKey::from_str(keypair.pk.as_hex_string().as_str())
+                .unwrap()
+                .as_hex_string(),
+            keypair.pk.as_hex_string()
+        );
+        assert_eq!(
+            PublicKey::from_str(keypair.pk.as_hex_string().as_str())
+                .unwrap()
+                .compress(),
+            PublicKeyBytes::from_str(keypair.pk.as_hex_string().as_str()).unwrap()
+        );
+
         println!(
             "pk:\t{}\nsk:\t{}",
             keypair.pk.as_hex_string(),
--- src/deposit/gen.rs
+++ src/deposit/gen.rs
@@ -1,17 +1,45 @@
+use bls::blst_implementations::{SecretKey, SignatureBytes};
 use clap::Parser;
+use consensus_types::{ChainSpec, DepositMessage, Hash256, SignedRoot};
+use hex;
 
 #[derive(Parser, Debug)]
 pub struct Gen {
     #[arg(long)]
-    name: String,
+    seckey: String,
     #[arg(long)]
-    blskey: String,
+    withdrawal_credentials: String,
     #[arg(long)]
-    amount: u32,
+    amount: u64,
+}
+
+fn remove_hex_prefix(hex_string: &str) -> &str {
+    if hex_string.starts_with("0x") {
+        &hex_string[2..]
+    } else {
+        hex_string
+    }
+}
+
+fn decode_0xhex(hex_string: &str) -> Vec<u8> {
+    return hex::decode(remove_hex_prefix(hex_string)).unwrap();
 }
 
 impl Gen {
     pub fn run(&self) {
-        println!("hello world: {}", self.name);
+        let sk = SecretKey::deserialize(decode_0xhex(self.seckey.as_str()).as_slice()).unwrap();
+        let d = DepositMessage {
+            pubkey: sk.public_key().compress(),
+            withdrawal_credentials: Hash256::from_slice(
+                hex::decode(remove_hex_prefix(self.withdrawal_credentials.as_str()))
+                    .unwrap()
+                    .as_slice(),
+            ),
+            amount: self.amount,
+        };
+
+        let spec = ChainSpec::default();
+        let msg = d.signing_root(spec.get_deposit_domain());
+        println!("{}", SignatureBytes::from(sk.sign(msg)).to_string());
     }
 }
